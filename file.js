2ec453b5580c8aaec53baa5f2a13865b8536de0d
Abuzar Haider <abuzarsarwary3@gmail.com>
2021-12-17 18:23:57 +0500

    First Commit

---
+++ file.js
@@ -0,0 +1,48 @@
+function two(){
+    let a = document.getElementById('two');
+    a.style.display = "none";
+    document.getElementById('four').style.display = "inline-block"
+    
+};
+function four(){
+    let a = document.getElementById('four');
+    a.style.display = "none";
+    document.getElementById('six').style.display = "inline-block"
+    
+};
+function six(){
+    let a = document.getElementById('six');
+    a.style.display = "none";
+    document.getElementById('eight').style.display = "inline-block"
+    
+};
+function eight(){
+    let a = document.getElementById('eight');
+    a.style.display = "none";
+    document.getElementById('one').style.display = "inline-block"
+    
+};
+function one(){
+    let a = document.getElementById('one');
+    a.style.display = "none";
+    document.getElementById('nine').style.display = "inline-block"
+    
+};
+function nine(){
+    let a = document.getElementById('nine');
+    a.style.display = "none";
+    document.getElementById('three').style.display = "inline-block"
+    
+};
+function three(){
+    let a = document.getElementById('three');
+    a.style.display = "none";
+    document.getElementById('seven').style.display = "inline-block"
+    
+};
+function seven(){
+    let a = document.getElementById('seven');
+    a.style.display = "none";
+    document.getElementById('two').style.display = "inline-block"
+    
+};
